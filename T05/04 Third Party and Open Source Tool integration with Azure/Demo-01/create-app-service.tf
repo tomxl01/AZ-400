2038f7e9e82b9329a3fe2cfaf54ab8c5801056be
Alexander Pajer <alexander.pajer@integrations.at>
2020-06-18 07:06:16 +0200

    ansible

--- T05/04 Third Party and Open Source Tool integration with Azure/Demo-01/create-app-service.tf
+++ T05/04 Third Party and Open Source Tool integration with Azure/Demo-01/create-app-service.tf
@@ -6,32 +6,32 @@ provider "azurerm" {
     features {}
 }
 
-resource "azurerm_resource_group" "slotDemo" {
-    name = "slotDemoResourceGroup"
-    location = "westus2"
+resource "azurerm_resource_group" "az400-t05-terraform" {
+    name = "az400-t05-terraform"
+    location = "westeurope"
 }
 
-resource "azurerm_app_service_plan" "slotDemo" {
-    name                = "slotAppServicePlan"
-    location            = azurerm_resource_group.slotDemo.location
-    resource_group_name = azurerm_resource_group.slotDemo.name
+resource "azurerm_app_service_plan" "az400-t05-terraform" {
+    name                = "terraformPlan"
+    location            = azurerm_resource_group.az400-t05-terraform.location
+    resource_group_name = azurerm_resource_group.az400-t05-terraform.name
     sku {
         tier = "Standard"
         size = "S1"
     }
 }
 
-resource "azurerm_app_service" "slotDemo" {
-    name                = "slotAppService"
-    location            = azurerm_resource_group.slotDemo.location
-    resource_group_name = azurerm_resource_group.slotDemo.name
-    app_service_plan_id = azurerm_app_service_plan.slotDemo.id
+resource "azurerm_app_service" "az400-t05-terraform" {
+    name                = "terraformApp1234"
+    location            = azurerm_resource_group.az400-t05-terraform.location
+    resource_group_name = azurerm_resource_group.az400-t05-terraform.name
+    app_service_plan_id = azurerm_app_service_plan.az400-t05-terraform.id
 }
 
-resource "azurerm_app_service_slot" "slotDemo" {
-    name                = "slotAppServiceSlotOne"
-    location            = azurerm_resource_group.slotDemo.location
-    resource_group_name = azurerm_resource_group.slotDemo.name
-    app_service_plan_id = azurerm_app_service_plan.slotDemo.id
-    app_service_name    = azurerm_app_service.slotDemo.name
+resource "azurerm_app_service_slot" "az400-t05-terraform" {
+    name                = "terraformApp1234SlotOne"
+    location            = azurerm_resource_group.az400-t05-terraform.location
+    resource_group_name = azurerm_resource_group.az400-t05-terraform.name
+    app_service_plan_id = azurerm_app_service_plan.az400-t05-terraform.id
+    app_service_name    = azurerm_app_service.az400-t05-terraform.name
 }
